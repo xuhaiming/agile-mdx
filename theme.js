b11c77cabad0fafd700138c9141c6113b3ea0771
Xu Haiming <haiming12322@qq.com>
2019-08-26 21:50:06 +0800

    Update theme

--- theme.js
+++ theme.js
@@ -4,5 +4,23 @@ export default {
   ...theme,
   colors: {
     background: '#f1f5f8'
+  },
+  h1: {
+    fontSize: '1.5em'
+  },
+  h3: {
+    fontSize: '1em'
+  },
+  p: {
+    fontSize: '1em'
+  },
+  ul: {
+    fontSize: '1em'
+  },
+  ol: {
+    fontSize: '1em'
+  },
+  li: {
+    fontSize: '1em'
   }
 }
